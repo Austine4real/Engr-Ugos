88052566a7c09571d32bb853d71e1c97511c82bf
Ugbodaga Augustine <83676555+Austine4real@users.noreply.github.com>
2022-12-20 16:46:30 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,19 @@
+// Initialize and add the
+// function initMap() {
+//     const loc = {lat:6.500210, lng:3.302700 };
+//  const map = new google.maps.Map(document.querySelector("map"), 
+//  { 
+//     zoom: 14,
+//     center:loc
+//   });
+//   const marker = new google.map.marker({position: loc, map: map});
+// }
+// [END maps_map_simple]
+// let map;
+
+// function initMap() {
+//   map = new google.maps.Map(document.getElementById("map"), {
+//     center: { lat: -34.397, lng: 150.644 },
+//     zoom: 8,
+//   });
+// }
